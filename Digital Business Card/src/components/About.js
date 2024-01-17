c271fcafbfba444e20e2f0004baf8045f1f8f27a
Parishri Shah <136893851+Parishri07@users.noreply.github.com>
2024-01-18 00:28:19 +0530

    Add files via upload

---
+++ Digital Business Card/src/components/About.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "../style.css"
+
+export default function About(){
+    return(
+        <div>
+           <h4 className="heading">About</h4>
+           <p className="text">I am a frontend developer with a particular interest in making things simple and automating daily tasks. I am always looking for new things to learn and implement my skills to real world problems.</p>
+        </div>
+    )
+}
---
+++ Digital Business Card/src/components/Interests.js
@@ -0,0 +1,11 @@
+import React from "react"
+import "../style.css"
+
+export default function Interests(){
+     return(
+        <div>
+          <h4 className="heading">Interests</h4>
+          <p className="text">Space enthusiast. Internet fanatic. Travel geek.</p>
+        </div>
+    )
+}
