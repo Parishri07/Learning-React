f83bdeab35a663034a77b8c688b00d8bbcbc995e
Parishri Shah <136893851+Parishri07@users.noreply.github.com>
2024-01-27 14:48:12 +0530

    Create data.js

---
+++ Airbnb website clone/src/data.js
@@ -0,0 +1,28 @@
+import React from "react"
+
+export default [
+            {
+                id: 1,
+                title: "Life Lessons with Katie Zaferes",
+                price: 136,
+                coverImg: "Katie-Zaferes.png",
+                stats: {
+                    rating: 5.0,
+                    reviewCount: 6
+                },
+                location: "USA",
+                openSpots: 0      
+            },
+            {
+                id: 2,
+                title: "Learn wedding photography",
+                price: 125,
+                coverImg: "Katie-Zaferes.png",
+                stats: {
+                    rating: 5.0,
+                    reviewCount: 30
+                },
+                location: "Online",
+                openSpots: 27      
+            }
+       ]
