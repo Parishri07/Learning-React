e3f38e94c8323148c7b4ca48c8e2f31efde60b39
Parishri Shah <136893851+Parishri07@users.noreply.github.com>
2024-02-03 14:31:42 +0530

    Update index.js

---
+++ Forms in React/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import Form from "./Form"
+
+function App() {
+  return (
+   <Form />
+  )
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />); 
