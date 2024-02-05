d42739e6e28dedbc1ccada4bd0f41c5f700e8d3c
Parishri Shah <136893851+Parishri07@users.noreply.github.com>
2024-02-05 20:14:18 +0530

    Update Navbar.js

--- react-facts--app/src/components/Navbar.js
+++ react-facts--app/src/components/Navbar.js
@@ -2,12 +2,16 @@ import React from "react"
 import reactLogo from "../images/react-logo.png"
 import "../style.css"
 
-export default function Navbar() {
+export default function Navbar(props) {
     return (
-        <nav className="nav-bar">
-          <img src={reactLogo} className="logo"/>
+        <nav className={`nav-bar ${props.mode}`} >
+          <img src="../images/react-logo.png" className="logo"/>
           <h3 className="nav-heading">ReactFacts</h3>
           <h4 className="nav-content">React Course - Project 1</h4>
+          <label className="toggle-switch" >
+            <input type="checkbox" onClick={props.changeMode}/>
+            <span className="slider" ></span>
+         </label>
         </nav>
     )
 }
