b8022d1ea108e784e30b107bc1c244edce8242d8
Parishri Shah <136893851+Parishri07@users.noreply.github.com>
2024-02-05 20:16:27 +0530

    Update App.js

--- react-facts--app/src/App.js
+++ react-facts--app/src/App.js
@@ -3,10 +3,23 @@ import Navbar from "./components/Navbar"
 import MainContent from "./components/MainContent"
 
 export default function App(){
+    const [mode, setMode] = React.useState("dark") 
+    
+    const toggleMode = () => {
+            if(mode === "light"){
+               setMode("dark")
+               document.body.style.backgroundColor = 'black'
+            }
+            else{
+                setMode("light")
+               document.body.style.backgroundColor = 'white'
+            }
+    }
+         
     return (
         <div className="container">
-        <Navbar />
-        <MainContent />
+        <Navbar mode={mode} changeMode={toggleMode}/>
+        <MainContent mode={mode}/>
         </div>
     )
 }
